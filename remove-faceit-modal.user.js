571ac5cb5f2d0784375351510e49ebf19ef6c20e
Finn <contact@bechinka.eu>
2022-08-11 01:49:59 +0200

    added dev to name

--- remove-faceit-modal.user.js
+++ remove-faceit-modal.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         remove faceit modal
+// @name         [dev] remove faceit modal
 // @namespace    https://www.faceit.com/
 // @version      0.3
 // @description  replace modal profile link with normal profile link
