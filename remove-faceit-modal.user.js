97c388886535e8c09629a4250b9c44b4b1adaaaa
Finn <contact@bechinka.eu>
2022-08-12 20:06:11 +0200

    removed [dev]

--- remove-faceit-modal.user.js
+++ remove-faceit-modal.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         [dev] remove faceit modal
+// @name         remove faceit modal
 // @namespace    https://www.faceit.com/
 // @version      0.3
 // @description  replace modal profile link with normal profile link
