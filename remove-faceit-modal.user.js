ba40ca6dd09501a0a70edc33ab59d7f17db50dc7
Finn <contact@bechinka.eu>
2022-08-15 00:03:26 +0200

    added a button to modal profiles
    click it to open the non modal profile in a new tab

--- remove-faceit-modal.user.js
+++ remove-faceit-modal.user.js
@@ -16,21 +16,43 @@
 
 (function () {
     "use strict";
+    const faceit_icon =
+        "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://faceit.com&size=32";
     let elements;
     let old_elements;
+    let open_profile;
     window.setInterval(() => {
-        if (window.location.href.includes("faceit.com/en/players-modal/")) {
-            const shadows = Array.from(document.querySelectorAll("*"))
-                .map((el) => el.shadowRoot)
-                .filter(Boolean);
-            shadows.forEach((s) => {
-                const res = s.querySelectorAll("div");
-                res.forEach((e) => {
-                    if (e.firstChild.data == "Member since") {
-                        console.log("WEOW");
+        if (window.location.href.includes("players-modal")) {
+            if (!open_profile) {
+                // find the shadow root(s) (very cringe)
+                const shadows = Array.from(document.querySelectorAll("*"))
+                    .map((el) => el.shadowRoot)
+                    .filter(Boolean);
+                shadows.forEach((s) => {
+                    elements = s.querySelectorAll("button");
+                    if (elements != old_elements) {
+                        old_elements = elements;
+                        elements.forEach((e) => {
+                            if (e.lastChild.data == "Share") {
+                                let img = document.createElement("img");
+                                img.src = faceit_icon;
+                                img.style.marginTop = "15px";
+                                open_profile = document.createElement("a");
+                                open_profile.appendChild(img);
+                                open_profile.title =
+                                    "open this profile in a new tab";
+                                let url = window.location.href;
+                                url = url.replace("players-modal", "players");
+                                open_profile.href = url;
+                                open_profile.target = "_blank";
+                                open_profile.style.marginTop = "15px";
+
+                                e.parentNode.parentNode.append(open_profile);
+                            }
+                        });
                     }
                 });
-            });
+            }
         } else {
             elements = document.querySelectorAll("[href*='/players-modal/']");
             if (elements !== old_elements) {
