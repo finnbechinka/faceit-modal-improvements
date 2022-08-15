f8345fb1944dc04ee90235791d4988cbc1968d1b
Finn <contact@bechinka.eu>
2022-08-15 10:57:50 +0200

    incremet version number

--- remove-faceit-modal.user.js
+++ remove-faceit-modal.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         remove faceit modal
 // @namespace    https://www.faceit.com/
-// @version      1.0
+// @version      1.1
 // @description  replace modal profile link with normal profile link
 // @author       shaker
 // @match        *://www.faceit.com/*
