6b6747fb4ed6686edb8dd09827972094e403ec2c
Finn <contact@bechinka.eu>
2022-08-16 12:45:26 +0200

    refactoring

--- remove-faceit-modal.user.js
+++ remove-faceit-modal.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         remove faceit modal
 // @namespace    https://www.faceit.com/
-// @version      1.1.2
+// @version      1.2.2
 // @description  replace modal profile link with normal profile link
 // @author       shaker
 // @match        *://www.faceit.com/*
@@ -17,10 +17,67 @@
 (function () {
     "use strict";
 
-    let elements;
-    let old_elements;
     let old_url;
-    let open_profiles = [];
+    let my_elements = [];
+
+    function remove_my_elements() {
+        my_elements.forEach((open_profile) => {
+            let parent = open_profile.parentNode;
+            if (parent) {
+                parent.removeChild(parent.lastChild);
+                parent.removeChild(parent.lastChild);
+                parent.removeChild(parent.lastChild);
+            }
+        });
+        my_elements = [];
+    }
+
+    function add_open_tab_button(current_url) {
+        if (my_elements.length != 0) {
+            remove_my_elements();
+        }
+        // find the shadow root(s) (very cringe)
+        const shadows = Array.from(document.querySelectorAll("*"))
+            .map((el) => el.shadowRoot)
+            .filter(Boolean);
+        shadows.forEach((s) => {
+            let elements = s.querySelectorAll("button");
+            elements.forEach((e) => {
+                if (e.lastChild.data == "Share") {
+                    const div = e.parentNode.parentNode;
+
+                    let button = e.cloneNode(true);
+                    button.removeChild(button.firstChild);
+                    button.lastChild.data = "OPEN IN NEW TAB";
+
+                    let link = document.createElement("a");
+                    link.title = "open this profile in a new tab";
+                    let url = current_url.replace(
+                        "/players-modal/",
+                        "/players/"
+                    );
+                    link.href = url;
+                    link.target = "_blank";
+                    link.style.textDecoration = "none";
+                    link.appendChild(button);
+
+                    div.lastChild.append(document.createElement("br"));
+                    div.lastChild.append(document.createElement("br"));
+                    div.lastChild.append(link);
+                    my_elements.push(link);
+                }
+            });
+        });
+    }
+
+    function change_profile_link() {
+        const elements = document.querySelectorAll("[href*='/players-modal/']");
+        elements.forEach((element) => {
+            const link = element.getAttribute("href");
+            const new_link = link.replace("/players-modal/", "/players/");
+            element.setAttribute("href", new_link);
+        });
+    }
 
     // Select the node that will be observed for mutations
     const targetNode = document.body;
@@ -30,86 +87,19 @@
 
     // Callback function to execute when mutations are observed
     const callback = (mutationList, observer) => {
-        /*
-        for (const mutation of mutationList) {
-            if (mutation.type === "childList") {
-                console.log("A child node has been added or removed.");
-            } else if (mutation.type === "attributes") {
-                console.log(
-                    `The ${mutation.attributeName} attribute was modified.`
-                );
-            }
-        }
-        */
-        console.log("observer");
         const current_url = window.location.href;
 
-        old_url = current_url;
-        open_profiles.forEach((open_profile) => {
-            let div = open_profile.parentNode;
-            if (div) {
-                div.removeChild(div.lastChild);
-                div.removeChild(div.lastChild);
-                div.removeChild(div.lastChild);
-            }
-        });
-        open_profiles = [];
+        if (current_url.includes("/players-modal/")) {
+            add_open_tab_button(current_url);
+        }
 
-        if (current_url.includes("players-modal")) {
-            if (
-                (old_url.includes("/players/") && open_profiles.length < 2) ||
-                open_profiles.length < 1
-            ) {
-                // find the shadow root(s) (very cringe)
-                const shadows = Array.from(document.querySelectorAll("*"))
-                    .map((el) => el.shadowRoot)
-                    .filter(Boolean);
-                shadows.forEach((s) => {
-                    elements = s.querySelectorAll("button");
-                    if (elements != old_elements) {
-                        old_elements = elements;
-                        elements.forEach((e) => {
-                            if (e.lastChild.data == "Share") {
-                                const div = e.parentNode.parentNode;
+        if (current_url.includes("/room/")) {
+            change_profile_link();
+        }
 
-                                let button = e.cloneNode(true);
-                                button.removeChild(button.firstChild);
-                                button.lastChild.data = "OPEN IN NEW TAB";
-
-                                let link = document.createElement("a");
-                                link.title = "open this profile in a new tab";
-                                let url = current_url.replace(
-                                    "players-modal",
-                                    "players"
-                                );
-                                link.href = url;
-                                link.target = "_blank";
-                                link.style.textDecoration = "none";
-                                link.appendChild(button);
-
-                                div.lastChild.append(
-                                    document.createElement("br")
-                                );
-                                div.lastChild.append(
-                                    document.createElement("br")
-                                );
-                                div.lastChild.append(link);
-                                open_profiles.push(link);
-                            }
-                        });
-                    }
-                });
-            }
-        } else {
-            elements = document.querySelectorAll("[href*='/players-modal/']");
-            if (elements !== old_elements) {
-                old_elements = elements;
-                elements.forEach((element) => {
-                    const link = element.getAttribute("href");
-                    const new_link = link.replace("players-modal", "players");
-                    element.setAttribute("href", new_link);
-                });
-            }
+        if (current_url != old_url) {
+            old_url = current_url;
+            remove_my_elements();
         }
     };
 
